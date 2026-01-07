7fbe8ea384921f4b2628c05e12c7f87f1db8c7a8
sujalsoni19 <sujal19soni2004@gmail.com>
2026-01-08 00:40:26 +0530

    add getWatchHistory function and update user routes

--- youtube_backend/src/controllers/user.controller.js
+++ youtube_backend/src/controllers/user.controller.js
@@ -428,4 +428,55 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
 
 })
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUsercoverimg, getUserChannelProfile };
+const getWatchHistory = asyncHandler(async(req,res) => {
+
+    const user = await User.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(req.user._id)
+        }
+      },
+      {
+        $lookup: {
+          from: "videos",
+          localField: "watchHistory",
+          foreignField: "_id",
+          as: "watchHistory",
+          pipeline: [
+            { 
+              $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner"
+              }
+            }
+          }
+          ]
+        }
+      }
+    ])
+
+    return res
+    .status(200)
+    .json(
+      new Apiresponse(200,user[0].WatchHistory,"Watch History fetched successfully")
+    )
+})
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUsercoverimg, getUserChannelProfile, getWatchHistory };

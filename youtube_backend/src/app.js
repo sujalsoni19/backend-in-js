fba1d8d6a2e300a4e22fe907f1eca86025ec1f90
sujalsoni19 <sujal19soni2004@gmail.com>
2026-01-23 18:07:49 +0530

    feat: implement subscription management with toggle, retrieval, and channel subscriber functionalities

--- youtube_backend/src/app.js
+++ youtube_backend/src/app.js
@@ -2,57 +2,53 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }));
 
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 //routes import
 
-import userRouter from "./routes/user.route.js"
+import userRouter from "./routes/user.route.js";
 
 //routes declaration
 //http://localhost:8000/api/v1/users/register
-app.use("/api/v1/users",userRouter)
+app.use("/api/v1/users", userRouter);
 
+import videoRouter from "./routes/video.route.js";
 
-import videoRouter from "./routes/video.route.js"
+app.use("/api/v1/videos", videoRouter);
 
-app.use("/api/v1/videos",videoRouter)
+import tweetRouter from "./routes/tweet.route.js";
 
+app.use("/api/v1/tweets", tweetRouter);
 
-import tweetRouter from "./routes/tweet.route.js"
-
-app.use("/api/v1/tweets",tweetRouter)
-
-
-import commentRouter from "./routes/comment.route.js"
+import commentRouter from "./routes/comment.route.js";
 
 app.use("/api/v1/videos/:videoId/comments", commentRouter);
 
-
-import playlistRouter from "./routes/playlist.route.js"
+import playlistRouter from "./routes/playlist.route.js";
 
 app.use("/api/v1/playlists", playlistRouter);
 
+import likeRouter from "./routes/like.route.js";
 
-import likeRouter from "./routes/like.route.js"
+app.use("/api/v1/likes", likeRouter);
 
-app.use("/api/v1/likes", likeRouter)
+import subscriptionRouter from "./routes/subscription.route.js";
 
+app.use("/api/v1/subscriptions",subscriptionRouter);
 
-
-
-
-
-export { app }
+export { app };
